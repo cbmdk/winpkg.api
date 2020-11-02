9617d6f6e8c8c0c6932d89f0c107f767d88f5ff8
Christian B. Mortensen <cbm@nisportal.com>
2020-11-02 16:10:48 +0100

    no message

--- winpkg.node/pkg2mongo.js
+++ winpkg.node/pkg2mongo.js
@@ -1,6 +1,23 @@
 const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/winstall")
 
-const readFiles = require("node-read-yaml-files");
+// const readFiles = require("node-read-yaml-files");
+const yaml = require('js-yaml');
+const fs   = require('fs');
 
-readFiles("./node_modules").then((docs) => console.log(docs));
+// Get document, or throw exception on error
+try {
+  const doc = yaml.safeLoad(fs.readFileSync('C:\\code\\packages\\manifests\\3T\\Robo3T\\1.3.1.yaml', 'utf8'));
+  console.log(doc);
+} catch (e) {
+  console.log(e);
+}
+// const fs = require('fs');
+// const testFolder = 's:\\temp';
+
+// fs.readdir(testFolder, (err, files) => {
+//   if (err) return console.log(err);
+//   files.forEach(file => {
+//     console.log(file);
+//   });
+// });
