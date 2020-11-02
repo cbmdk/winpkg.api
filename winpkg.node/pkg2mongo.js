235f555d95b43de327edca1710034b79939bb769
Christian B. Mortensen <cbm@nisportal.com>
2020-11-02 16:26:49 +0100

    no message

--- winpkg.node/pkg2mongo.js
+++ winpkg.node/pkg2mongo.js
@@ -1,16 +1,54 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/winstall")
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/winstall");
 
 // const readFiles = require("node-read-yaml-files");
-const yaml = require('js-yaml');
-const fs   = require('fs');
+const yaml = require("js-yaml");
+const fs = require("fs");
+const glob = require("glob")
 
-// Get document, or throw exception on error
-try {
-  const doc = yaml.safeLoad(fs.readFileSync('C:\\code\\packages\\manifests\\3T\\Robo3T\\1.3.1.yaml', 'utf8'));
-  console.log(doc);
-} catch (e) {
-  console.log(e);
+var packages = [];
+
+const dir = 'C:\\code\\packages\\manifests\*.yaml';
+
+glob(dir, function (err, files) {
+    // files is an array of filenames.
+    // If the `nonull` option is set, and nothing
+    // was found, then files is ["**/*.js"]
+    // er is an error object or null.
+    if(err){
+        throw err;
+    }
+    console.log(files)
+    // files.foreach(file =>{
+    //     console.log(file);
+    // })
+  })
+
+// fs.readdir(dir, (err,files) => {
+//     if(err){
+//         throw err;
+//     }
+//     files.forEach(element => {
+//         console.log(element);
+//     });
+// });
+
+
+function ReadAndStoreFiles(theFile) {
+  // Get document, or throw exception on error
+  try {
+    const doc = yaml.safeLoad(
+      fs.readFileSync(
+        theFile,
+        "utf8"
+      )
+    );
+    packages.push(doc);
+    
+  } catch (e) {
+    console.log(e);
+  }
+  return packages;
 }
 // const fs = require('fs');
 // const testFolder = 's:\\temp';
